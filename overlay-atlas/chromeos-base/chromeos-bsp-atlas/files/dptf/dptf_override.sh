d2753413bd3c0071d8c7b30408813936324976cf
Puthikorn Voravootivat <puthik@chromium.org>
2019-09-18 10:25:13 -0700

    Atlas: Update dptf setting
    BUG=b:140769572,b:139895589
    TEST=test in lab on Atlas
    
    Change-Id: Ie8fbef294f7ad9bcac88c53281f5b32acd2ee3fe
    Signed-off-by: Puthikorn Voravootivat <puthik@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/1810626
    Reviewed-by: Nick Sanders <nsanders@chromium.org>

--- overlay-atlas/chromeos-base/chromeos-bsp-atlas/files/dptf/dptf_override.sh
+++ overlay-atlas/chromeos-base/chromeos-bsp-atlas/files/dptf/dptf_override.sh
@@ -7,6 +7,7 @@
 # Select dptf configuration based on the following criteria.
 # - CPU model name
 # - dram part number prefix
+# - Amount of dram
 #
 # Documentation: go/atlas-dptf-override
 #
@@ -18,17 +19,20 @@ dptf_get_override() {
 
   cpu_model="$(uname -p | grep -o "[im][357]")"
   dram_part="$(mosys memory spd print id -s part_number | grep -om1 ^..)"
+  dram_size="$(head -1 /proc/meminfo | grep -oE ' (8|16)' | cut -c 2-)"
 
-  cpu_dram="$(echo "${cpu_model}"-"${dram_part}")"
+  cpu_dram="$(echo "${cpu_model}"-"${dram_part}-${dram_size}")"
 
   dptf_file=""
   case "${cpu_dram}" in
-    m3-K4) dptf_file="50_57_54_51_49_55_53.bin" ;;
-    m3-MT) dptf_file="50_57_54_51_49_55_53.bin" ;;
-    i5-K4) dptf_file="50_57_54_51_49_55_53.bin" ;;
-    i5-MT) dptf_file="50_57_54_51_49_55_53.bin" ;;
-    i7-K4) dptf_file="50_57_54_53_51_55_54.bin" ;;
-    i7-MT) dptf_file="50_57_55_52_51_56_53.bin" ;;
+    m3-K4-8)  dptf_file="0088_0987.bin" ;;
+    m3-MT-8)  dptf_file="0088_0987.bin" ;;
+    i5-K4-8)  dptf_file="0088_0987.bin" ;;
+    i5-MT-8)  dptf_file="0088_0987.bin" ;;
+    i5-K4-16) dptf_file="0088_0987.bin" ;;
+    i5-MT-16) dptf_file="3210_2008.bin" ;;
+    i7-K4-16) dptf_file="3210_2008.bin" ;;
+    i7-MT-16) dptf_file="3311_2109.bin" ;;
   esac
 
   if [ ! -f "/etc/dptf/${dptf_file}" ]; then
