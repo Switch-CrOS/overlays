8714cf94895700f0c820b43d1b1a09d15e49679b
Puthikorn Voravootivat <puthik@chromium.org>
2019-09-23 14:43:35 -0700

    Atlas: Update dptf setting
    Update the dptf config based on current test result.
    Also, fix style error in dptf_override script
    
    BUG=b:140769572,b:139895589
    TEST=Test in thermal chamber
    
    Change-Id: I2e843eed7ff9346d23252ff722f2b6724edfc5c2
    Signed-off-by: Puthikorn Voravootivat <puthik@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/1819987
    Reviewed-by: Matthew Blecker <matthewb@chromium.org>

--- overlay-atlas/chromeos-base/chromeos-bsp-atlas/files/dptf/dptf_override.sh
+++ overlay-atlas/chromeos-base/chromeos-bsp-atlas/files/dptf/dptf_override.sh
@@ -6,38 +6,37 @@
 #
 # Select dptf configuration based on the following criteria.
 # - CPU model name
-# - dram part number prefix
-# - Amount of dram
+# - DRAM part number prefix
+# - DRAM capacity
 #
-# Documentation: go/atlas-dptf-override
+# Documentation: http://go/atlas-dptf-override
 #
 
 dptf_get_override() {
-  local dram_part
-  local cpu_model
-  local dptf_file
+  local dram_part=
+  local cpu_model=
+  local memtotal_kib=
+  local memtotal_gib=
+  local dptf_file=
 
   cpu_model="$(uname -p | grep -o "[im][357]")"
   dram_part="$(mosys memory spd print id -s part_number | grep -om1 ^..)"
-  dram_size="$(head -1 /proc/meminfo | grep -oE ' (8|16)' | cut -c 2-)"
+  memtotal_kib="$(grep '^MemTotal:' /proc/meminfo | grep -oE '[0-9]+')"
+  # Atlas should have even GiB of RAM.  Round up to nearest even number.
+  memtotal_gib="$(( (((memtotal_kib >> 20) + 1) / 2) * 2 ))"
 
-  cpu_dram="$(echo "${cpu_model}"-"${dram_part}-${dram_size}")"
+  cpu_dram="$(echo "${cpu_model}-${dram_part}-${memtotal_gib}")"
 
-  dptf_file=""
   case "${cpu_dram}" in
-    m3-K4-8)  dptf_file="0088_0987.bin" ;;
-    m3-MT-8)  dptf_file="0088_0987.bin" ;;
-    i5-K4-8)  dptf_file="0088_0987.bin" ;;
-    i5-MT-8)  dptf_file="0088_0987.bin" ;;
-    i5-K4-16) dptf_file="0088_0987.bin" ;;
-    i5-MT-16) dptf_file="3210_2008.bin" ;;
-    i7-K4-16) dptf_file="3210_2008.bin" ;;
-    i7-MT-16) dptf_file="3311_2109.bin" ;;
+    m3-K4-8)  dptf_file="0987_8765.bin" ;;
+    m3-MT-8)  dptf_file="1098_0987.bin" ;;
+    i5-K4-8)  dptf_file="1098_9876.bin" ;;
+    i5-MT-8)  dptf_file="1098_0987.bin" ;;
+    i5-K4-16) dptf_file="1098_0987.bin" ;;
+    i5-MT-16) dptf_file="1199_0886.bin" ;;
+    i7-K4-16) dptf_file="2200_0987.bin" ;;
+    i7-MT-16) dptf_file="1199_1098.bin" ;;
   esac
-
-  if [ ! -f "/etc/dptf/${dptf_file}" ]; then
-    dptf_file=""
-  fi
 
   echo "${dptf_file}"
 }
