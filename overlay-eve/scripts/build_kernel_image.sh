34cec2fa09a730ea9197412423f029da76577560
Eugene Nemkov <eugene.nemkov@intel.com>
2019-10-01 15:46:44 -0700

    eve: Enable GuC/HuC during boot by default
    Add kernel command line parameters to load and enable GuC and HuC during boot.
    
    BUG=chromium:965320
    TEST=
    1. GuC functional test:
        a. Boot OS to GUI, interact with UI.
        b. Enable ssh and connect to DUT.
        c. # cat /proc/cmdline
           Expect 'i915.enable_guc_loading=1 i915.enable_guc_submission=0'
           in kernel parameters list
        d. # cat /run/debugfs_gpu/i915_guc_load_status
                path: i915/kbl_guc_ver9_39.bin
                fetch: SUCCESS
                load: SUCCESS
                version wanted: 9.39
                version found: 9.39
    	    < . . .>
        e. # while true; do cat /run/debugfs_gpu/i915_guc_info; sleep 1; done
           Observe that a counter of GuC sumbissions to render ring is increasing.
    
    2. GuC regression test: Set i915.enable_guc_loading=0 and
       i915.enable_guc_submission=0 in the kernel config. Boot OS to GUI.
    
    3. HuC functional test. HuC is used by libva/iHD only when CBR low power mode
       requested.
        a. Boot OS with default kernel parameters - i915.enable_guc_loading=1
           i915.enable_guc_submission=0
    
        b. # cat /run/debugfs_gpu/i915_huc_load_status
            HuC firmware status:
    	path: i915/kbl_huc_ver02_00_1810.bin
    	fetch: SUCCESS
    	load: SUCCESS
    	< . . .>
    
        c. Install ffmpeg 4.1 or higher. Prepare some video clips and decode it
           to raw yuv420 format.
    
        d. # stop ui
           In /run/debugfs_gpu/i915_guc_info check that submission counters are
           not increasing.
    
        e. Prepare encoding test script:
           # cat encoding-test.sh
           ffmpeg -hwaccel vaapi -vaapi_device /dev/dri/renderD128 \
           -f rawvideo -pix_fmt yuv420p -s:v $SIZE -r:v $FPS -i $SRC \
           -vf 'format=nv12,hwupload' -c:v h264_vaapi -low_power $LOW_POWER \
           -profile:v main -g 30 $BR_OPTIONS -y $DST
    
        f. Encoding test #1. Set video parameters according to the source file and
           force CBR mode by setting bitrate==maxrate:
           # export SRC=src.yuv
           # export DST=out.mp4
           # export SIZE=3120x2160
           # export FPS=29.97
           # export LOW_POWER=1
           # export BR_OPTIONS="-b:v 2M -maxrate 2M"
           # sh encoding-test.sh
    
           In i915_guc_info output observe incrementing of bsd ring submission
           counter only.
           Check video parameters of the output file using 'ffprobe $DST'
           Play out.mp4 - compare to original file.
           Repeat with other video test samples. A set of samples is at
           the discretion of a validation engineer.
    
    4. Encoding regression test.
        a. Encoding test #2. CQP low power mode.
           Use settings from the test 3.f. Change only bit rate:
           # export BR_OPTIONS=
           # sh encoding-test.sh
    
           Check the result in the same way as in the previous test.
    
        b. Encoding test #3. VBR high power mode.
           Use settings from the test 3.f. Change:
           # export LOW_POWER=0
           # export BR_OPTIONS=
           # sh encoding-test.sh
    
           In i915_guc_info output observe incrementing of render and bsd rings
           submission counters.
           Check the result in the same way as in the previous test.
    
        c. Set 'i915.enable_guc_loading=0 i915.enable_guc_submission=0' in the
           kernel command line, reboot and repeat tests 3.f, 4.a and 4.b.
           Encoding test #1 shall fail - libVA will report unsupported mode.
           Results of tests #2 and #3 shall be the same except
           i915_guc_info - it will show zeroes.
    
    5. GuC/HuC endurance test.
        a. Boot two eve targets with GuC/HuC enabled, connect to network and login.
        b. Start hangout session between targets, leave it for the next 24 hours.
        c. Session shall be alive.
    
    Cq-Depend: chromium:1850654
    Change-Id: I09d66bc8166c735feae59fde8d18f8cdfe66ec48
    Signed-off-by: Eugene Nemkov <eugene.nemkov@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/1834687
    Commit-Queue: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>

--- overlay-eve/scripts/build_kernel_image.sh
+++ overlay-eve/scripts/build_kernel_image.sh
@@ -7,4 +7,8 @@
 modify_kernel_command_line() {
   # Don't disable the ability to run VMs.
   echo "disablevmx=off" >> "$1"
+
+  # Enable GuC/HuC
+  echo "i915.enable_guc_loading=1" >> "$1"
+  echo "i915.enable_guc_submission=0" >> "$1"
 }
