ec2594a23e0eeb057cd6bab2109490e900598a3c
Xudong He <xudong.he@mediatek.com>
2019-10-14 10:47:19 +0800

    kukui: mt8183: camera: update metadata
    update MTK_SENSOR_INFO_SENSITIVITY_RANGE,MTK_SENSOR_INFO_PHYSICAL_SIZE
    and MTK_LENS_INFO_AVAILABLE_FOCAL_LENGTHS
    
    TEST=CTS verifier FOV Calibaration pass
    BUG=b:141268197
    
    Change-Id: I7b5e4aad63be69c4c126dbdd29604106a933903b
    Signed-off-by: Xudong He <xudong.he@mediatek.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/1857850
    Tested-by: Xudong He <xudong.he@mediatek.corp-partner.google.com>
    Reviewed-by: Hsu Wei-Cheng <mojahsu@chromium.org>
    Commit-Queue: Hsu Wei-Cheng <mojahsu@chromium.org>

--- overlay-kukui/media-libs/mtk-sensor-metadata/files/imgsensor_metadata/ov8856_mipi_raw/config_static_metadata.lens.ov8856mipiraw.h
+++ overlay-kukui/media-libs/mtk-sensor-metadata/files/imgsensor_metadata/ov8856_mipi_raw/config_static_metadata.lens.ov8856mipiraw.h
@@ -22,7 +22,7 @@ STATIC_METADATA2_BEGIN(DEVICE, LENS, SENSOR_DRVNAME_OV8856_MIPI_RAW)
 //------------------------------------------------------------------------------
 //==========================================================================
 CONFIG_METADATA_BEGIN(MTK_LENS_INFO_AVAILABLE_APERTURES)
-CONFIG_ENTRY_VALUE(2.8f, MFLOAT)
+CONFIG_ENTRY_VALUE(2.0f, MFLOAT)
 CONFIG_METADATA_END()
 
 //==========================================================================
@@ -31,7 +31,7 @@ CONFIG_ENTRY_VALUE(0.0f, MFLOAT)
 CONFIG_METADATA_END()
 //==========================================================================
 CONFIG_METADATA_BEGIN(MTK_LENS_INFO_AVAILABLE_FOCAL_LENGTHS)
-CONFIG_ENTRY_VALUE(4.52f, MFLOAT)
+CONFIG_ENTRY_VALUE(2.48f, MFLOAT)
 CONFIG_METADATA_END()
 //==========================================================================
 CONFIG_METADATA_BEGIN(MTK_LENS_INFO_AVAILABLE_OPTICAL_STABILIZATION)
