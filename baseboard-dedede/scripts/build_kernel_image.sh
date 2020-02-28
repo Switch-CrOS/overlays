aeb9a17b44ab799b7faa32c59f159ea52dee480d
Sudarshan S <sudarshan.s@intel.com>
2020-02-28 12:17:30 +0530

    baseboard-dedede: Add board specific image build script
    BUG=b:147649258
    TEST=`./build_packages --board=dedede && ./build_image --board dedede`
    is  successful.
    `cat ~/trunk/src/build/images/dedede/latest/config.txt` has the
    kernel argument i915.enable_guc=2
    On DUT: `cat /proc/cmdline` and check if the kernel argument
    i915.enable_guc=2 is available
    
    Change-Id: Ie0293526581fa4f88f9f46f43a48da99d7f059a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/2081632
    Reviewed-by: Kirtika Ruchandani <kirtika@chromium.org>
    Commit-Queue: Kirtika Ruchandani <kirtika@chromium.org>
    Tested-by: Kirtika Ruchandani <kirtika@chromium.org>

---
+++ baseboard-dedede/scripts/build_kernel_image.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+modify_kernel_command_line() {
+
+  # Enable GuC loading
+  echo "i915.enable_guc=2" >> "$1"
+}
