4e52c5a98cc3f6d01a7d70040890f8ee809f46ab
chromeos-ci-prod <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>
2020-03-19 21:22:35 -0700

    chromeos-config-bsp: Automatic uprev to 3e69d3cf1c3a63873ac3f12f214d095a0a481df8.
    Generated by PUpr, see https://cr-buildbucket.appspot.com/build/8885338875722788016 for job details.
    
    BUG=None
    
    TEST=CQ
    
    Cq-Cl-Tag: pupr:chromeos-base/chromeos-config-bsp
    Change-Id: I0eb01aa82f04b282c3a9d6e15fc6291d8d305c4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/2111140
    Bot-Commit: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>
    Commit-Queue: Andrew Lamb <andrewlamb@chromium.org>

--- overlay-dedede/chromeos-base/chromeos-config-bsp/files/generated/config.c
+++ overlay-dedede/chromeos-base/chromeos-config-bsp/files/generated/config.c
@@ -12,6 +12,16 @@ static struct config_map all_configs[] = {
               .signature_id = "waddledoo"}},
 
     {.platform_name = "dedede",
+     .firmware_name_match = "Waddledee",
+     .sku_id = 2147483647,
+     .customization_id = "",
+     .whitelabel_tag = "",
+     .info = {.brand = "",
+              .model = "waddledee",
+              .customization = "waddledee",
+              .signature_id = "waddledee"}},
+
+    {.platform_name = "dedede",
      .firmware_name_match = "Jslrvp",
      .sku_id = 2147483647,
      .customization_id = "",
@@ -23,6 +33,6 @@ static struct config_map all_configs[] = {
 };
 
 const struct config_map *cros_config_get_config_map(int *num_entries) {
-  *num_entries = 2;
+  *num_entries = 3;
   return &all_configs[0];
 }
