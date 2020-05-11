78a72c1276ce0b245e3530d73d445012c46ad371
Cheng-Han Yang <chenghan@chromium.org>
2020-05-11 07:51:21 +0000

    Revert "Zork: Changes console to ttyS0"
    This reverts commit fcc2320b31c99bb129bd37af02eee3ddd789fe07.
    
    Reason for revert: This change is causing factory shim not showing any texts. Before figuring out the solution in factory shim, we revert this change due to an urgent need of signed factory shim.
    
    Original change's description:
    > Zork: Changes console to ttyS0
    > 
    > Zork was using mmio uart during early development. This is no
    > longer required and was causing conflicts on the console.
    > 
    > BUG=b:152079780
    > TEST=build image, boot, check command line args:
    >      "earlyprintk=ttyS0,115200n8 console=tty1 console=ttyS0,115200n8"
    > 
    > Change-Id: I1f5917f7ccb017f1eb95a0e7026d428fca417da5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/2180929
    > Commit-Queue: Rob Barnes <robbarnes@google.com>
    > Tested-by: Rob Barnes <robbarnes@google.com>
    > Auto-Submit: Rob Barnes <robbarnes@google.com>
    > Reviewed-by: Raul E Rangel <rrangel@chromium.org>
    
    Bug: b:152079780, b:156203259
    Change-Id: I0f79884c518cecb27754a814541eb7741d0ec9fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/2192524
    Commit-Queue: Cheng-Han Yang <chenghan@chromium.org>
    Reviewed-by: Daniel Kurtz <djkurtz@chromium.org>
    Tested-by: Cheng-Han Yang <chenghan@chromium.org>

--- overlay-zork/scripts/board_specific_setup.sh
+++
@@ -1,7 +0,0 @@
-# Copyright 2020 The Chromium OS Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-# This will add console=ttyS0 kernel cmdline flag, thus rerouting
-# dmesg output to ttyS0 (serial port).
-FLAGS_enable_serial=ttyS0
