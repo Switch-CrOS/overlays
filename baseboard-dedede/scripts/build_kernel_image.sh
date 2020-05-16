e4a1c32f08c7726401cdf723d576f3b23f806d82
Divagar Mohandass <divagar.mohandass@intel.com>
2020-05-16 00:05:55 +0000

    Revert "baseboard-dedede: Limit C state transition"
    This reverts commit eed94b4ab883d6503ec1ec68bf9de173757dd810.
    
    Reason for revert: C state transition can be enabled now with
    latest Pre ES SOC.
    
    BUG=b:154773379
    TEST=cat /sys/kernel/debug/pmc_core/package_cstate_show
    
    Cq-Depend: chrome-internal:2974119
    Change-Id: I67a4be2f30d8d143be647715d03f9a2df2d64e6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/2203170
    Tested-by: Divagar Mohandass <divagar.mohandass@intel.com>
    Reviewed-by: Justin TerAvest <teravest@chromium.org>

--- baseboard-dedede/scripts/build_kernel_image.sh
+++ baseboard-dedede/scripts/build_kernel_image.sh
@@ -9,9 +9,6 @@ modify_kernel_command_line() {
   # Enable GuC loading
   echo "i915.enable_guc=2" >> "$1"
 
-  # TODO(b/154773379): Workaround for frequency scaling issues
-  echo "intel_idle.max_cstate=1" >> "$1"
-
   # Enable S0ix logging using GSMI
   echo "gsmi.s0ix_logging_enable=1" >> "$1"
 
