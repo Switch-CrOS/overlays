89b8d20ed5b3f4c31b162c75e15252243d18e81b
Rob Barnes <robbarnes@google.com>
2020-05-19 12:59:00 -0600

    Zork: Adds 115200n8 to console override.
    Linux defaults to 9600. Without this option specified, the serial output
    will switch to 9600 during some portions of kernel boot.
    
    BUG=b:152079780
    TEST=Booted 10 times, less junk output
    
    Change-Id: I6f0fb018ab35458cf23e851e17cb09d02094a1c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/2209355
    Tested-by: Rob Barnes <robbarnes@google.com>
    Auto-Submit: Rob Barnes <robbarnes@google.com>
    Reviewed-by: Kangheui Won <khwon@chromium.org>
    Commit-Queue: Kangheui Won <khwon@chromium.org>

--- overlay-zork/scripts/build_kernel_image.sh
+++ overlay-zork/scripts/build_kernel_image.sh
@@ -5,7 +5,7 @@
 # found in the LICENSE file.
 
 modify_kernel_command_line() {
-  sed -i -E -e 's/^console=$/console=uart,mmio32,0xfedc9000 /' "$1"
+  sed -i -E -e 's/^console=$/console=uart,mmio32,0xfedc9000,115200n8 /' "$1"
   echo "amdgpu.abmlevel=4" >> "$1"
   echo "amdgpu.dcfeaturemask=0x8" >> "$1"
 }
