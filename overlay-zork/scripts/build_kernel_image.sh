421dfd09b42ed9d58e7a50feb907f117e81527ae
Kangheui Won <khwon@chromium.org>
2020-06-01 11:04:12 +1000

    Zork: limit number of uarts to 2
    Zork doesn't use ttyS2 and ttyS3, but serial8250 driver tries to
    initialize it and results in unstable serial.
    set nr_uarts to 2 so kernel will only initialize ttyS0 and ttyS1.
    
    BUG=b:157858890
    TEST=check `dmesg | grep Serial`, see Serial: 8250/16550 driver, 2 ports, ...
    
    Change-Id: I9b9be9de38c294ad10aa4a01d85c497928394182
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/2224454
    Tested-by: Kangheui Won <khwon@chromium.org>
    Commit-Queue: Daniel Kurtz <djkurtz@chromium.org>
    Reviewed-by: Raul E Rangel <rrangel@chromium.org>

--- overlay-zork/scripts/build_kernel_image.sh
+++ overlay-zork/scripts/build_kernel_image.sh
@@ -6,6 +6,7 @@
 
 modify_kernel_command_line() {
   sed -i -E -e 's/^console=$/console=uart,mmio32,0xfedc9000,115200n8 /' "$1"
+  echo "8250_core.nr_uarts=2" >> "$1"
   echo "amdgpu.abmlevel=4" >> "$1"
   echo "amdgpu.dcfeaturemask=0x8" >> "$1"
 }
