215007fc04158efcb3aea34d0e98b71387ccf607
Bhanu Prakash Maiya <bhanumaiya@chromium.org>
2020-10-14 19:41:31 -0700

    Remove 8250_core.nr_uarts=2 from kernel build
    BUG=b:170906253
    TEST=none
    
    Change-Id: I3d8a4bb2b991a85bb2d20f7813bf8f5b22191775
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/2473483
    Commit-Queue: Bhanu Prakash Maiya <bhanumaiya@google.com>
    Tested-by: Bhanu Prakash Maiya <bhanumaiya@google.com>
    Reviewed-by: George Engelbrecht <engeg@google.com>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Rob Barnes <robbarnes@google.com>
    Auto-Submit: Bhanu Prakash Maiya <bhanumaiya@google.com>

--- overlay-zork/scripts/build_kernel_image.sh
+++ overlay-zork/scripts/build_kernel_image.sh
@@ -12,7 +12,6 @@
 # See crrev.com/i/216896 as an example.
 
 modify_kernel_command_line() {
-  echo "8250_core.nr_uarts=2" >> "$1"
   echo "amdgpu.abmlevel=4" >> "$1"
   echo "amdgpu.dcfeaturemask=0x8" >> "$1"
 
