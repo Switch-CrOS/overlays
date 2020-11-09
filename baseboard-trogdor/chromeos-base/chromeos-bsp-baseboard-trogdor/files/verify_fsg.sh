ca374294ff1b88903c145c8c36bb5df8f1a8eb9f
Evan Green <evgreen@chromium.org>
2020-11-09 08:51:44 -0800

    baseboard-trogdor: Quote cros_debug?1 in verify_fsg.sh
    Mike pointed out that quotes are needed to avoid path expansion.
    
    BUG=b:172392522
    TEST=None
    
    Change-Id: I372ccd01048327a3e6fc9c974b4090ba7dff2925
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/2526288
    Tested-by: Evan Green <evgreen@chromium.org>
    Auto-Submit: Evan Green <evgreen@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>

--- baseboard-trogdor/chromeos-base/chromeos-bsp-baseboard-trogdor/files/verify_fsg.sh
+++ baseboard-trogdor/chromeos-base/chromeos-bsp-baseboard-trogdor/files/verify_fsg.sh
@@ -153,7 +153,7 @@ verify_fsg() {
       logwarn "Fuses are unprogrammed. LTE will need developer assistance."
 
     # Allow a hash mismatch in dev/test images.
-    elif crossystem cros_debug?1 ; then
+    elif crossystem "cros_debug?1" ; then
       logwarn "FSG hash check failed, forgiven in developer mode."
 
     # For other errors, blank out the FSG in case the eMMC boot partition
