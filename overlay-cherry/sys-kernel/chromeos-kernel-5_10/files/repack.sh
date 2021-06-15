612287eeb47b3ab67cc6d366e80b1ddf90680866
Fei Shao <fshao@chromium.org>
2021-06-15 19:24:56 +0800

    cherry: Improve repack.sh
    Make the script easier to be customized for a board along with minor
    improvements.
    
    BUG=b:185092438
    TEST=bash repack.sh
         bash repack.sh 2781499
         bash repack.sh 2781499/62
    
    Change-Id: I9b5a6b27e52d80aeb78d985056f685d189fdd80b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/2963932
    Reviewed-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Commit-Queue: Fei Shao <fshao@chromium.org>
    Tested-by: Fei Shao <fshao@chromium.org>

--- overlay-cherry/sys-kernel/chromeos-kernel-5_10/files/repack.sh
+++ overlay-cherry/sys-kernel/chromeos-kernel-5_10/files/repack.sh
@@ -3,19 +3,38 @@
 # Copyright 2020 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-#
-# This script repacks the kernel squash and updates the ebuild automatically
-# according to the given patchset url or the patch id.
-# If the patchset number is unspecified, this script will use the latest
-# patchset to generate the kernel squash.
-#
-# Examples:
-#     sh repack.sh https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2206575/12
-#     sh repack.sh https://crrev.com/c/2206575/
-#     sh repack.sh CL:2206575/12
-#     sh repack.sh 2206575
 
 set -e
+
+HELP_TEXT=$(cat <<END
+This script repacks the kernel squash and updates the ebuild automatically
+according to the given patchset url or the patch id.
+If the patchset number is unspecified, this script will use the latest
+patchset to generate the kernel squash.
+
+Examples:
+    sh repack.sh https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2206575/12
+    sh repack.sh https://crrev.com/c/2206575/
+    sh repack.sh CL:2206575/12
+    sh repack.sh 2206575
+END
+)
+
+# Variables that customize the script for a board.
+BOARD=cherry
+MAJOR_VER=5
+MINOR_VER=10
+BUG_ID=185092438
+
+script="$(realpath "${BASH_SOURCE[0]}")"
+kernel_dir="${script%overlays*}third_party/kernel/v${MAJOR_VER}.${MINOR_VER}"
+ebuild_dir="$(dirname "${script}")/.."
+
+patch_id=""
+patchset=""
+base=""
+tree=""
+version=""
 
 info() {
   echo -e "\e[1;32m$*\e[0m"
@@ -44,12 +63,12 @@ generate_new_squash() {
   git fetch "https://chromium.googlesource.com/chromiumos/third_party/kernel" \
     refs/changes/"${patch_id: -2}"/"${patch_id}"/"${patchset}"
 
-  base="$(git merge-base cros/chromeos-5.10 FETCH_HEAD)"
+  base="$(git merge-base "cros/chromeos-${MAJOR_VER}.${MINOR_VER}" FETCH_HEAD)"
   tree="$(git cat-file -p "${base}" | sed -n 's/^tree //p' | head -n 1)"
 
   info "Generating new squash..."
   git diff --full-index "${base}" FETCH_HEAD > \
-    "${ebuild_dir}"/files/cherry-tot.patch
+    "${ebuild_dir}/files/${BOARD}-tot.patch"
 
   popd
 }
@@ -66,10 +85,10 @@ update_ebuild() {
   old_tree="$(grep CROS_WORKON_TREE ./*.ebuild | cut -d \" -f 2)"
   version="$(find ./*.ebuild | sed -Ee 's/.*\.([0-9]*).ebuild/\1/')"
 
-  sed -i s/"${old_base}"/"${base}"/g chromeos-kernel-5_10-0.0."${version}".ebuild
-  sed -i s/"${old_tree}"/"${tree}"/g chromeos-kernel-5_10-0.0."${version}".ebuild
-  mv chromeos-kernel-5_10-0.0."${version}".ebuild \
-    chromeos-kernel-5_10-0.0."$((version + 1))".ebuild
+  sed -i s/"${old_base}"/"${base}"/g "chromeos-kernel-${MAJOR_VER}_${MINOR_VER}-0.0.${version}.ebuild"
+  sed -i s/"${old_tree}"/"${tree}"/g "chromeos-kernel-${MAJOR_VER}_${MINOR_VER}-0.0.${version}.ebuild"
+  mv "chromeos-kernel-${MAJOR_VER}_${MINOR_VER}-0.0.${version}.ebuild" \
+    "chromeos-kernel-${MAJOR_VER}_${MINOR_VER}-0.0.$((version + 1)).ebuild"
 
   popd
 }
@@ -101,31 +120,33 @@ commit_change() {
 
   local commit_msg
   read -r -d \" commit_msg <<END
-cherry: update ebuild with private patch for kernel 5.10
+${BOARD}: update ebuild with private patch for kernel ${MAJOR_VER}.${MINOR_VER}
 
 Apply ${tot_name} #${patchset} (CL:${patch_id}/${patchset}, ${tot_commit_id}) on commit ${base:0:13}.
 
+The squash is created by running \`bash repack.sh $1\` which does the following:
+
 git fetch cros
 
-git merge-base cros/chromeos-5.10 ${tot_commit_id}
+git merge-base cros/chromeos-${MAJOR_VER}.${MINOR_VER} ${tot_commit_id}
 > ${base}
 
-git cat-file -p ${base} | sed -n 's/tree //p'
+git cat-file -p ${base} | sed -n 's/^tree //p' | head -n 1
 > ${tree}
 
 diff generated with
-git diff --full-index ${base} ${tot_commit_id} > cherry-tot.patch
-by running \`sh repack.sh $1\`
+git diff --full-index ${base} ${tot_commit_id} > ${BOARD}-tot.patch
 
-BUG=b:185092438
-TEST=cros-workon-cherry stop chromeos-kernel-5_10
-     emerge-cherry chromeos-kernel-5_10
+
+BUG=b:${BUG_ID}
+TEST=cros-workon-${BOARD} stop chromeos-kernel-${MAJOR_VER}_${MINOR_VER}
+     emerge-${BOARD} chromeos-kernel-${MAJOR_VER}_${MINOR_VER}
 "
 END
 
-  git add chromeos-kernel-5_10-0.0."${version}".ebuild
-  git add chromeos-kernel-5_10-0.0."$((version + 1))".ebuild
-  git add files/cherry-tot.patch
+  git add "chromeos-kernel-${MAJOR_VER}_${MINOR_VER}-0.0.${version}.ebuild"
+  git add "chromeos-kernel-${MAJOR_VER}_${MINOR_VER}-0.0.$((version + 1)).ebuild"
+  git add files/"${BOARD}"-tot.patch
   git add files/scmversion.patch
   git commit -m "${commit_msg}"
   git commit --amend
@@ -133,21 +154,19 @@ END
   popd
 }
 
-script="$(realpath "${BASH_SOURCE[0]}")"
-kernel_dir="${script%overlays*}third_party/kernel/v5.10"
-ebuild_dir="$(dirname "${script}")/.."
+main() {
+  if [[ $# != 1 ]]; then
+    echo "${HELP_TEXT}"
+    exit 0
+  fi
 
-patch_id=""
-patchset=""
-base=""
-tree=""
-version=""
+  generate_new_squash "$1"
+  update_ebuild
+  update_scmversion
+  commit_change "$1"
 
-generate_new_squash "$1"
-update_ebuild
-update_scmversion
-commit_change "$1"
+  info "Please run \`cros-workon-${BOARD} stop chromeos-kernel-${MAJOR_VER}_${MINOR_VER} &&"\
+    "emerge-${BOARD} chromeos-kernel-${MAJOR_VER}_${MINOR_VER}\` in chroot to verify the result."
+}
 
-info "Please run \`cros-workon-cherry stop chromeos-kernel-5_10 &&"\
-  "emerge-cherry chromeos-kernel-5_10\` in chroot to verify the result."
-
+main "$@"
