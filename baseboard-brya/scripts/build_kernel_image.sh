9e8be5d7705ef3b335868c45526f95ffb446a7df
Daniel Winkler <danielwinkler@google.com>
2021-11-16 14:01:15 -0800

    Brya: Direct the swiotlb argument correctly
    First patch defined swiotlb but didn't put it into the cmdline arguments
    correctly. This fixes it.
    
    BUG=b:201020414
    TEST=No errors on 5G driver load.
    
    Change-Id: Ic97e703c4488399f257c3a7395e2311c20b3b619
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/3287003
    Reviewed-by: Rajat Jain <rajatja@google.com>
    Tested-by: Rajat Jain <rajatja@google.com>
    Commit-Queue: Daniel Winkler <danielwinkler@google.com>

--- baseboard-brya/scripts/build_kernel_image.sh
+++ baseboard-brya/scripts/build_kernel_image.sh
@@ -35,5 +35,5 @@ modify_kernel_command_line() {
 
   # The 5G driver requires a lot of swiotlb buffers (b/201020414)
   # So increase the swiotlb slots from default 32768 (64MB) to 65536 (128MB)
-  echo "swiotlb=65536"
+  echo "swiotlb=65536" >> "$1"
 }
