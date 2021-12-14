a9472aa7d3ddbe7972fb6c525548d5d14b7810b6
Mike Frysinger <vapier@chromium.org>
2021-12-14 00:22:14 -0500

    beaglebone: use new bootloader hook
    The bootloader script has a new hook that runs with the EFI dir already
    mounted for us, so simplify this hook to speed things up and to avoid
    mounting the disk image directly ourselves.  This avoids kernel flakes
    where the kernel doesn't flush its loopback caches properly.
    
    BUG=b:207034476
    TEST=`./build_image --board=beaglebone` installs the files into the EFI partition
    
    Cq-Depend: chromium:3336559
    Change-Id: I3134e233e495302447d06e82f6b1f91051a63924
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/3336285
    Tested-by: Mike Frysinger <vapier@chromium.org>
    Auto-Submit: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: Ram Chandrasekar <rchandrasekar@google.com>
    Commit-Queue: Ram Chandrasekar <rchandrasekar@google.com>

--- overlay-beaglebone/scripts/board_specific_setup.sh
+++ overlay-beaglebone/scripts/board_specific_setup.sh
@@ -4,27 +4,13 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# Downloads and installs the MLO and copies u-boot to the ESP.
-# This is used combined with a hybrid MBR to allow booting
-# a Beaglebone from microSD only.
-install_beaglebone_bootloader() {
-  local efi_offset_sectors=$(partoffset "$1" 12)
-  local efi_size_sectors=$(partsize "$1" 12)
-  local efi_offset=$(( efi_offset_sectors * 512 ))
-  local efi_size=$(( efi_size_sectors * 512 ))
-  local mount_opts=loop,offset=${efi_offset},sizelimit=${efi_size}
-  local efi_dir=$(mktemp -d)
+# Installs the MLO and copies u-boot to the ESP.  This is used combined with
+# a hybrid MBR to allow booting a Beaglebone from microSD only.
+board_update_bootloaders() {
+  local sysroot="$1"
+  local efi_dir="$2"
 
-  sudo mount -o "${mount_opts}"  "$1" "${efi_dir}"
-
-  sudo cp "${BOARD_ROOT}/firmware/u-boot.img" "${efi_dir}/"
-  sudo cp "${BOARD_ROOT}/firmware/MLO" "${efi_dir}/"
-  sudo cp "${BOARD_ROOT}/firmware/uEnv.txt" "${efi_dir}/"
-
-  sudo umount "${efi_dir}"
-  rmdir "${efi_dir}"
-}
-
-board_setup() {
-  install_beaglebone_bootloader "$1"
+  sudo cp "${sysroot}/firmware/u-boot.img" "${efi_dir}/" || exit
+  sudo cp "${sysroot}/firmware/MLO" "${efi_dir}/" || exit
+  sudo cp "${sysroot}/firmware/uEnv.txt" "${efi_dir}/" || exit
 }
