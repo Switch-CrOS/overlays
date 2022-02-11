73f8a635873ac2ee02fc8637a261a52f12e43ef3
Johnny Li <johnny_li@wistron.corp-partner.google.com>
2022-02-11 20:29:05 +0800

    Arcada: Add board specific script to get panel ID for flash Wacom FW.
    Arcada use Wacom Touch screen with different firmwares to support
    different panels. As a result, we need a way to identify the correct
    firmware to update. The solution is to probe VID_PID from
    eDP panel's EDID as a identifier then transfer to Wacom HWID which
    used to search file names of firmware blobs.
    
    BUG=b:207306169
    TEST=verify touch firmware update on Arcada
    
    Change-Id: Id10e7f8d4176747c4cce3fd1a4313e840faa9a12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/3468925
    Reviewed-by: Jesper Lin <jesper_lin@wistron.corp-partner.google.com>
    Tested-by: Johnny Li <johnny_li@wistron.corp-partner.google.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Commit-Queue: Johnny Li <johnny_li@wistron.corp-partner.google.com>

--- overlay-sarien/chromeos-base/chromeos-bsp-sarien/files/arcada/get_board_specific_wacom_hwid.sh
+++ overlay-sarien/chromeos-base/chromeos-bsp-sarien/files/arcada/get_board_specific_wacom_hwid.sh
@@ -46,6 +46,9 @@ main() {
     "ae0d-1382") # ae0d(INO)
       echo "2d1f_4946"  # wacom hardware_id = VID_PID
     ;;
+    "e509-08dd") # e509(BOE)
+      echo "2d1f_4a01"  # wacom hardware_id = VID_PID
+    ;;
     *)
       ## Unknown hardware_id, don't ouput anything.
   esac
