e77e67d1a933509bd6fdf1f0d2c73d4510f96619
George D Sworo <george.d.sworo@intel.com>
2022-03-10 12:04:48 -0800

    baseboard-brya: Temporarily disable PSR2
    This is a WA until b:216826833 is root caused and fixed
    Options: (0=disabled, 1=enable up to PSR1, 2=enable up to PSR2)
    
    Issue:  screen flickering on select display panels during video playback
    
    Impact: This WA increases power during local video playback by upto
    70mWatts (Full screen) and 140mW (non-full-screen)
    
    BUG=b:216826833
    TEST=Basic boot on an ADL device and in command line check: cat
    /syst/kernel/debug/dri/0/i915_edp_psr_status
    
    Cq-Depend: chromium:3518132
    Signed-off-by: George D Sworo <george.d.sworo@intel.com>
    Change-Id: I5ebafab21a040281130a68a91452ae14a0b433e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/3518185
    Reviewed-by: Rajat Jain <rajatja@google.com>

--- baseboard-brya/scripts/build_kernel_image.sh
+++ baseboard-brya/scripts/build_kernel_image.sh
@@ -39,4 +39,9 @@ modify_kernel_command_line() {
 
   # Ensure internal devices are also in their own DMA domain,
   echo "intel_iommu=on" >> "$1"
+
+  # Disable PSR2 by default.
+  # (0=disabled, 1=enable up to PSR1, 2=enable up to PSR2)
+  # Temporary WA until b:216826833 is root caused and fixed
+  echo "i915.enable_psr=1" >> "$1"
 }
