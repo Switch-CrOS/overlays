df31e455812da8b94ff674df51adf1d9ae847af8
Derek Basehore <dbasehore@google.com>
2022-06-17 15:50:50 -0700

    build_kernel_image.sh: Change slp_s0_seeds to match other platforms
    The newer EC based SLP S0 watchdog uses a 15 second timeout to account
    for the serial console delaying SLP S0 assertion. This ups our initial
    timeout from 5 to match this on the various baseboards and overlays that
    set this.
    
    BUG=b:236322525
    TEST=suspend_stress_test on atlas
    
    Change-Id: Ic5e06f3401072ba7503d40d8ea0a9be3af639275
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/3712545
    Reviewed-by: YH Lin <yueherngl@chromium.org>
    Commit-Queue: Derek Basehore <dbasehore@chromium.org>
    Tested-by: Derek Basehore <dbasehore@chromium.org>
    Reviewed-by: Bob Moragues <moragues@chromium.org>

--- baseboard-krabbylake/scripts/build_kernel_image.sh
+++ baseboard-krabbylake/scripts/build_kernel_image.sh
@@ -16,7 +16,7 @@ modify_kernel_command_line() {
   echo "intel_idle.slp_s0_check=1" >> "$1"
 
   # Setup S0ix validation initial timeout for slp_s0_check
-  echo "intel_idle.slp_s0_seed=5" >> "$1"
+  echo "intel_idle.slp_s0_seed=15" >> "$1"
 
   # Don't disable the ability to run VMs.
   echo "disablevmx=off" >> "$1"
--- overlay-soraka/scripts/build_kernel_image.sh
+++ overlay-soraka/scripts/build_kernel_image.sh
@@ -17,7 +17,7 @@ modify_kernel_command_line() {
   echo "intel_idle.slp_s0_check=1" >> "$1"
 
   # Setup S0ix validation initial timeout for slp_s0_check
-  echo "intel_idle.slp_s0_seed=5" >> "$1"
+  echo "intel_idle.slp_s0_seed=15" >> "$1"
 
   # Enable S0ix logging using GSMI
   echo "gsmi.s0ix_logging_enable=1" >> "$1"
