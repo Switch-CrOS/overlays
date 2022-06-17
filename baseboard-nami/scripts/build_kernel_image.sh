958c6f74a8b41f3e1e5673e16bdae0f2f356904f
Derek Basehore <dbasehore@google.com>
2022-06-17 15:50:50 -0700

    nami: set slp_s0_seed in build_kernel_image.sh
    This configures slp_s0_seed to 15s to match the timeout on the EC SLP S0
    watchdog.
    
    BUG=b:236322525
    TEST=suspend_stress_test on nami device
    
    Change-Id: I09ede41781c6f23d888a02a05e5df03c08ea83ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/3711417
    Reviewed-by: Bob Moragues <moragues@chromium.org>
    Tested-by: Derek Basehore <dbasehore@chromium.org>
    Commit-Queue: Derek Basehore <dbasehore@chromium.org>
    Reviewed-by: YH Lin <yueherngl@chromium.org>

--- baseboard-nami/scripts/build_kernel_image.sh
+++ baseboard-nami/scripts/build_kernel_image.sh
@@ -18,6 +18,9 @@ modify_kernel_command_line() {
   # Enable S0ix logging using GSMI
   echo "gsmi.s0ix_logging_enable=1" >> "$1"
 
+  # Setup S0ix validation initial timeout for slp_s0_check
+  echo "intel_idle.slp_s0_seed=15" >> "$1"
+
   # Don't disable the ability to run VMs.
   echo "disablevmx=off" >> "$1"
 
