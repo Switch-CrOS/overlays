91ca9531fb7dd67512f509f00afeedd04e320c68
Brian Norris <briannorris@chromium.org>
2022-07-20 11:08:15 -0700

    baseboard-gru: Use TEO cpuidle governor
    Similar to crrev.com/c/2087859, but adapted to conditionally apply to
    newer kernels.
    
    This choice of cpuidle governor:
    (1) improves boot time ~200ms
    (2) improves VM filesystem test performance (vm.Virtiofs) by >20%
        (~8m30s -> ~6m30s)
    (3) negligibly impacts power tests (power_LoadTest.1hour, power_Idle)
    
    BUG=b:238655078
    TEST=see above
    
    Change-Id: I9fa70d96d72f56c894927955522cb9043d9fac31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/3777533
    Tested-by: Brian Norris <briannorris@chromium.org>
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Reviewed-by: Hsin-Yi Wang <hsinyi@chromium.org>
    Commit-Queue: Brian Norris <briannorris@chromium.org>

---
+++ baseboard-gru/scripts/build_kernel_image.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Copyright 2022 The ChromiumOS Authors.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# All kernel command line changes must update the security base lines in
+# the signer.  It rejects any settings it does not recognize and breaks the
+# build.  So any modify_kernel_command_line() function change here needs to be
+# reflected in ensure_secure_kernelparams.config.
+
+# See crrev.com/i/216896 as an example.
+
+modify_kernel_command_line() {
+  # TODO(b/231167263): drop conditionals once kernel 4.4 is gone. Kernel 4.4
+  # doesn't have TEO governor.
+
+  # FLAGS_* are set by parent script.
+  # shellcheck disable=SC2154
+  if ! has "kernel-4_4" "$("portageq-${FLAGS_board}" envvar USE)"; then
+    echo "cpuidle.governor=teo" >> "$1"
+  fi
+}
