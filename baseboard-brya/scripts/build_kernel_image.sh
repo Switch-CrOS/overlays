8db7a014685f9e495337db7599564a2c5f197044
Paz Zcharya <pazz@google.com>
2022-08-29 21:49:37 +0000

    baseboard-brya: Clarify comment to explain why PSR2 is disabled
    Impact: None
    
    BUG=b:243060986
    TEST=None
    
    Change-Id: I9452a3103d48f1779d1263e4352758aa5da5e608
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/3863965
    Tested-by: Paz Zcharya <pazz@google.com>
    Reviewed-by: Sven van Ashbrook <svenva@google.com>
    Reviewed-by: Rajat Jain <rajatja@google.com>
    Commit-Queue: Paz Zcharya <pazz@google.com>

--- baseboard-brya/scripts/build_kernel_image.sh
+++ baseboard-brya/scripts/build_kernel_image.sh
@@ -43,6 +43,10 @@ modify_kernel_command_line() {
   # Disable PSR2 by default.
   # (0=disabled, 1=enable up to PSR1, 2=enable up to PSR2)
   # Temporary WA until b:216826833 is root caused and fixed
-  #                and b:243060986 is root caused and fixed
+  #                and b:243060986 is root caused and fixed:
+  #                   Disable PSR2 to make panel work.
+  #                   Manufacturer: BOE (on some Redrix SKUs)
+  #                   Model: 2678
+  #                   Made in week 25 of 2021
   echo "i915.enable_psr=1" >> "$1"
 }
