0fe7311d31a18b8b02fe9617c7f61fa50f01261c
Sven van Ashbrook <svenva@chromium.org>
2023-03-11 02:33:58 +0000

    baseboard-rex: keep PSR2 disabled for all MTL platforms
    The Google Intel-Big-Core Team has decided to keep PSR2 disabled
    until further notice.
    
    BUG=b:272840957
    TEST=cat /sys/kernel/debug/dri/0/i915_edp_psr_status
         # check for absence of PSR2
    
    Change-Id: Ib179e25058a5b992efbdf84cdfd209d3e46cc901
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/4330658
    Tested-by: Paz Zcharya <pazz@google.com>
    Commit-Queue: Paz Zcharya <pazz@google.com>
    Reviewed-by: Paz Zcharya <pazz@google.com>

--- baseboard-rex/scripts/build_kernel_image.sh
+++ baseboard-rex/scripts/build_kernel_image.sh
@@ -33,6 +33,9 @@ modify_kernel_command_line() {
   # Disable xDomain protocol on the thunderbolt driver
   echo "xdomain=0" >> "$1"
 
+  # Disable PSR2 by default.
+  echo "i915.enable_psr=1" >> "$1"
+
   # Display kernel debug messages on the UART
   echo "earlyprintk=serial,ttyS0,115200n8 console=ttyS0,115200n8,keep " >> "$1"
 }
