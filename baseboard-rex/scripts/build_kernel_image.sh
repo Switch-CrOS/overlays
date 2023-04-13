45355a40e300acab9ae01c38d629197936a18875
Paz Zcharya <pazz@google.com>
2023-04-13 20:45:23 +0000

    baseboard_rex: enable iommu and disable dpt
    Add two parameters to the kernel commandline.
    "intel_iommu=on" enables Intel's iommu driver for IOMMU support.
    "i915.enable_dpt=0" disables dpt.
    Finally, add spaces in between command line arguments.
    
    BUG=b:278115758
    TEST=Device boots fine, IOMMU domains are enabled, dpt is disabled.
    
    Cq-Depend: chrome-internal:5763033
    Change-Id: Iec963244ca92c5effa498ca82fba61833e5435a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/4425569
    Commit-Queue: YH Lin <yueherngl@chromium.org>
    Reviewed-by: YH Lin <yueherngl@chromium.org>
    Tested-by: Paz Zcharya <pazz@google.com>
    Reviewed-by: Rajat Jain <rajatja@google.com>

--- baseboard-rex/scripts/build_kernel_image.sh
+++ baseboard-rex/scripts/build_kernel_image.sh
@@ -12,30 +12,38 @@
 # See crrev.com/i/216896 as an example.
 
 modify_kernel_command_line() {
-  # Might be helpful to preserve ramoops in extreme circumstances
-  echo "ramoops.ecc=1" >> "$1"
-
   # Avoid a cosmetic TPM error (Work around for b/113527055)
   sed -i -e '/tpm_tis.force/d' "$1"
-  echo "tpm_tis.force=0" >> "$1"
+  {
+    echo "tpm_tis.force=0 "
 
-  # Check for S0ix failures and show warnings on failures
-  echo "intel_pmc_core.warn_on_s0ix_failures=1" >> "$1"
+    # Might be helpful to preserve ramoops in extreme circumstances
+    echo "ramoops.ecc=1 "
 
-  # Enable Guc and Huc loading. When enable_guc is set to 3,
-  # it supports guc/huc loading and guc submission.
-  echo "i915.enable_guc=3" >> "$1"
+    # Check for S0ix failures and show warnings on failures
+    echo "intel_pmc_core.warn_on_s0ix_failures=1 "
 
-  # Enable power-saving display c states. Setting the value of 4
-  # enables up to DC6 with DC3C0.
-  echo "i915.enable_dc=4" >> "$1"
+    # Enable Guc and Huc loading. When enable_guc is set to 3,
+    # it supports guc/huc loading and guc submission.
+    echo "i915.enable_guc=3 "
 
-  # Disable xDomain protocol on the thunderbolt driver
-  echo "xdomain=0" >> "$1"
+    # Enable power-saving display c states. Setting the value of 4
+    # enables up to DC6 with DC3C0.
+    echo "i915.enable_dc=4 "
 
-  # Disable PSR2 by default.
-  echo "i915.enable_psr=1" >> "$1"
+    # Disable xDomain protocol on the thunderbolt driver
+    echo "xdomain=0 "
 
-  # Display kernel debug messages on the UART
-  echo "earlyprintk=serial,ttyS0,115200n8 console=ttyS0,115200n8,keep " >> "$1"
+    # Disable PSR2 by default.
+    echo "i915.enable_psr=1 "
+
+    # Enable Intel's iommu driver.
+    echo "intel_iommu=on "
+
+    # Disabling DPT (for context see b:270540659)
+    echo "i915.enable_dpt=0 "
+
+    # Display kernel debug messages on the UART
+    echo "earlyprintk=serial,ttyS0,115200n8 console=ttyS0,115200n8,keep "
+  } >> "$1"
 }
