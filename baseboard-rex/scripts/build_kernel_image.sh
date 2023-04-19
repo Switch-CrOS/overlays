41e15ce21ddb700a873b703b90c517bb856a9718
Paz Zcharya <pazz@google.com>
2023-04-19 17:17:38 +0000

    scripts/build_kernel_image: add "iommu=pt intremap=on"
    For MTL ES0/ES1, the recommended iommu settings are
    "intel_iommu=on,sm_on iommu=pt."
    Adding those settings as a temporary change until ES2 is prevalent.
    (See b:278761218#comment6 for more information.)
    Also, remove unnecessary spaces at the end of the echo commands.
    
    BUG=b:278761218, b:278115758
    TEST=check device boots and GPU doesn't hang
    
    Change-Id: I133d097152a631eb3706248e09404effa0070a94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/4448349
    Tested-by: Paz Zcharya <pazz@google.com>
    Commit-Queue: Rajat Jain <rajatja@google.com>
    Reviewed-by: Sukumar Ghorai <sukumar.ghorai@intel.com>
    Auto-Submit: Paz Zcharya <pazz@google.com>
    Reviewed-by: Rajat Jain <rajatja@google.com>
    Tested-by: Sukumar Ghorai <sukumar.ghorai@intel.com>

--- baseboard-rex/scripts/build_kernel_image.sh
+++ baseboard-rex/scripts/build_kernel_image.sh
@@ -15,35 +15,40 @@ modify_kernel_command_line() {
   # Avoid a cosmetic TPM error (Work around for b/113527055)
   sed -i -e '/tpm_tis.force/d' "$1"
   {
-    echo "tpm_tis.force=0 "
+    echo "tpm_tis.force=0"
 
     # Might be helpful to preserve ramoops in extreme circumstances
-    echo "ramoops.ecc=1 "
+    echo "ramoops.ecc=1"
 
     # Check for S0ix failures and show warnings on failures
-    echo "intel_pmc_core.warn_on_s0ix_failures=1 "
+    echo "intel_pmc_core.warn_on_s0ix_failures=1"
 
     # Enable Guc and Huc loading. When enable_guc is set to 3,
     # it supports guc/huc loading and guc submission.
-    echo "i915.enable_guc=3 "
+    echo "i915.enable_guc=3"
 
     # Enable power-saving display c states. Setting the value of 4
     # enables up to DC6 with DC3C0.
-    echo "i915.enable_dc=4 "
+    echo "i915.enable_dc=4"
 
     # Disable xDomain protocol on the thunderbolt driver
-    echo "xdomain=0 "
+    echo "xdomain=0"
 
     # Disable PSR2 by default.
-    echo "i915.enable_psr=1 "
+    echo "i915.enable_psr=1"
 
     # Enable Intel's iommu driver.
-    echo "intel_iommu=on "
+    # There are a few known issues with MTL ES0/ES1
+    # (see b/278761218#comment6 for more info),
+    # so instead of using "intel_iommu=on" we use the arguments
+    # below as a workaround. This will be removed in
+    # August 2023 (when everyone will be using ES2).
+    echo "intel_iommu=on,sm_on iommu=pt"
 
     # Disabling DPT (for context see b:270540659)
-    echo "i915.enable_dpt=0 "
+    echo "i915.enable_dpt=0"
 
     # Display kernel debug messages on the UART
-    echo "earlyprintk=serial,ttyS0,115200n8 console=ttyS0,115200n8,keep "
+    echo "earlyprintk=serial,ttyS0,115200n8 console=ttyS0,115200n8,keep"
   } >> "$1"
 }
