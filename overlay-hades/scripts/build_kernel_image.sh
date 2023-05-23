1a35f3e03c917073e7408f8bacf943b147c031e7
Vaibhav Vyas (SW) <vavyas@nvidia.corp-partner.google.com>
2023-05-23 01:23:42 -0700

    haydes: Enable iommu=pt in kernel commandline
    BUG=None
    TEST=Haydes overlay build
    
    Change-Id: Iffcebe10bc9eb2257f744b535833c972dc4ef7d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/4555005
    Auto-Submit: Vaibhav Vyas <vavyas@nvidia.corp-partner.google.com>
    Tested-by: Vaibhav Vyas <vavyas@nvidia.corp-partner.google.com>
    Commit-Queue: YH Lin <yueherngl@chromium.org>
    Reviewed-by: Dominik Behr <dbehr@chromium.org>
    Reviewed-by: YH Lin <yueherngl@chromium.org>

--- overlay-hades/scripts/build_kernel_image.sh
+++ overlay-hades/scripts/build_kernel_image.sh
@@ -40,8 +40,8 @@ modify_kernel_command_line() {
   # So increase the swiotlb slots from default 32768 (64MB) to 65536 (128MB)
   echo "swiotlb=65536" >> "$1"
 
-  # Ensure internal devices are also in their own DMA domain,
-  echo "intel_iommu=on" >> "$1"
+  # Ensure internal devices are also in their own DMA domain, enable IOMMU PT.
+  echo "intel_iommu=on iommu=pt" >> "$1"
 
   # Disable Nouveau.
   echo "nouveau_modeset=0" >> "$1"
