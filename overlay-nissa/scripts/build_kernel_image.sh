0f92b9a7443d26d8ab423b17d79650d840768c99
Nagi Marupaka <nmarupaka@google.com>
2023-05-25 17:13:53 +0000

    nissa: Increase the swiotlb size from 64MB->128MB for 5G
    Increase the SWIOTLB size to accommodate 5G driver requirements. These
    requirements are exposed because of Chromeos security requirements which
    requires using bounce buffers for any non page aligned DMA.
    
    https://buganizer.corp.google.com/issues/283927613#comment8 indicates that this change is indeed needed and verified on nissa boards.
    
    BUG=b:283927613
    TEST=No errors on 5G driver load.
    
    Change-Id: I83d1f31eb1f0d6ba2c1d94674c2478e00a229a06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/4567786
    Reviewed-by: Shou-Chieh Hsu <shouchieh@chromium.org>
    Tested-by: Nagi Marupaka <nmarupaka@google.com>
    Commit-Queue: Nagi Marupaka <nmarupaka@google.com>
    Reviewed-by: Daniel Winkler <danielwinkler@google.com>

--- overlay-nissa/scripts/build_kernel_image.sh
+++ overlay-nissa/scripts/build_kernel_image.sh
@@ -21,4 +21,8 @@ modify_kernel_command_line() {
 
   # Ensure internal devices are also in their own DMA domain
   echo "intel_iommu=on" >> "$1"
+
+  # The 5G driver requires a lot of swiotlb buffers (b/201020414)
+  # So increase the swiotlb slots from default 32768 (64MB) to 65536 (128MB)
+  echo "swiotlb=65536" >> "$1"
 }
