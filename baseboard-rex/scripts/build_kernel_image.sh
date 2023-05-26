9eaa7358b35b0f45d12e1d945ad5544992815b85
Rukun Mao <rmao@google.com>
2023-05-26 16:18:33 +0000

    Rex: Increase the swiotlb size from 64MB->128MB for 5G
    Increase the SWIOTLB size to accommodate 5G driver requirements.
    
    BUG=b:284465894
    TEST=No errors on 5G driver load.
    
    Change-Id: I45940e7f2154d10fa612c15cc353867f95a68ee5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/4569760
    Tested-by: Rukun Mao <rmao@google.com>
    Commit-Queue: Rukun Mao <rmao@google.com>
    Reviewed-by: Paz Zcharya <pazz@google.com>

--- baseboard-rex/scripts/build_kernel_image.sh
+++ baseboard-rex/scripts/build_kernel_image.sh
@@ -50,5 +50,9 @@ modify_kernel_command_line() {
 
     # Display kernel debug messages on the UART
     echo "earlyprintk=serial,ttyS0,115200n8 console=ttyS0,115200n8,keep"
+
+    # The 5G driver requires a lot of swiotlb buffers (b/284465894)
+    # So increase the swiotlb slots from default 32768 (64MB) to 65536 (128MB)
+    echo "swiotlb=65536"
   } >> "$1"
 }
