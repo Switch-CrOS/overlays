19686bc1cb1d092cb68a2b39de03870858b32da9
Shyam Sundar Radjacoumar <ssradjacoumar@google.com>
2023-12-18 15:49:04 -0800

    baseboard-brox: Add build_kernel_image.sh script
    - Add build_kernel_image.sh consistent with baseboard-brya.
    - Enable kernel debug messages on UART for power on.
    
    BUG=b:302708504
    TEST=CQ
    
    Change-Id: Ic1000bde352b35d6130f021a47fcd01f10e6cc44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/5133886
    Reviewed-by: Bob Moragues <moragues@chromium.org>
    Tested-by: Shyam Sundar Radjacoumar <ssradjacoumar@google.com>
    Commit-Queue: Shyam Sundar Radjacoumar <ssradjacoumar@google.com>
    Reviewed-by: YH Lin <yueherngl@chromium.org>

---
+++ baseboard-brox/scripts/build_kernel_image.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Copyright 2023 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# All kernel command line changes must update the security base lines in
+# the signer.  It rejects any settings it does not recognize and breaks the
+# build.  So any modify_kernel_command_line() function change here needs to be
+# reflected in ensure_secure_kernelparams.config.
+
+# See crrev.com/i/216896 as an example.
+
+modify_kernel_command_line() {
+  # Avoid a cosmetic TPM error (Work around for b/113527055)
+  sed -i -e '/tpm_tis.force/d' "$1"
+  {
+    echo "tpm_tis.force=0"
+
+    # Might be helpful to preserve ramoops in extreme circumstances
+    echo "ramoops.ecc=1"
+
+    # Check for S0ix failures and show warnings on failures
+    echo "intel_pmc_core.warn_on_s0ix_failures=1"
+
+    # Enable Guc and Huc loading. When enable_guc is set to 3,
+    # it supports guc/huc loading and guc submission.
+    echo "i915.enable_guc=3"
+
+    # Enable power-saving display c states. Setting the value of 4
+    # enables up to DC6 with DC3C0.
+    echo "i915.enable_dc=4"
+
+    # Disable xDomain protocol on the thunderbolt driver
+    echo "xdomain=0"
+
+    # The 5G driver requires a lot of swiotlb buffers (b/284465894)
+    # So increase the swiotlb slots from default 32768 (64MB) to 65536 (128MB)
+    echo "swiotlb=65536"
+
+    # Ensure internal devices are also in their own DMA domain,
+    echo "intel_iommu=on"
+
+    # Disable PSR2 by default.
+    # (0=disabled, 1=enable up to PSR1, 2=enable up to PSR2)
+    # Temporary WA until b:216826833 is root caused and fixed
+    #                and b:243060986 is root caused and fixed:
+    #                   Disable PSR2 to make panel work.
+    #                   Manufacturer: BOE (on some Redrix SKUs)
+    #                   Model: 2678
+    #                   Made in week 25 of 2021
+    echo "i915.enable_psr=1"
+
+    # Display kernel debug messages on the UART
+    echo "earlyprintk=serial,ttyS0,115200n8 console=ttyS0,115200n8,keep"
+  } >> "$1"
+}
