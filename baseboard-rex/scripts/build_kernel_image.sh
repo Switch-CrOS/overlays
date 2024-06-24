Switch-CrOS/overlays

commit 8065c5bc2e265431490acedbe84f6215181bdc02

rex: Disable DPT as a workaround for panic on wkae
Disabling DPT should avoid the panic on wake while Intel continues to
debug and address the root cause.

BUG=b:327056620
TEST=suspend_stress_test with low memory
TEST=verify kernel command line in image built from dry run

Cq-Depend: chrome-internal:7271614
Disallow-Recycled-Builds: rex-cq
Change-Id: I4161f9222cf4a9d990aad1fbef5a273e1453711b
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/5528732
Commit-Queue: Drew Davenport <ddavenport@chromium.org>
Auto-Submit: Drew Davenport <ddavenport@chromium.org>
Tested-by: Drew Davenport <ddavenport@chromium.org>
Reviewed-by: Samuel Jacob <samjaco@google.com>

baseboard-rex/scripts/build_kernel_image.sh
@@ -32,5 +32,9 @@ modify_kernel_command_line() {
     # The 5G driver requires a lot of swiotlb buffers (b/284465894)
     # So increase the swiotlb slots from default 32768 (64MB) to 65536 (128MB)
     echo "swiotlb=65536"
+
+    # DPT has been implicated in stability issues on resume. The feature is of
+    # limited value to ChromeOS, so disable it. See b:327056620
+    echo "i915.enable_dpt=0 " >> "$1"
   } >> "$1"
 }

commit ce9f63d56bf810a6b61a5fc9368992c2aad0c2cd

build_kernel_image.sh: Reformat the file to be consistent
Commit 8065c5b ("rex: Disable DPT as a workaround for panic on wkae")
mistakenly used the ">> $1" inside the curly brackets.
Similarly, commit 5649423 ("rex: build_kernel_image: disable tdp_mmu")
used the old mechanism and added a configuration outside the curly
brackets.

Fix those two CLs to get a consistent and correct file contents.

BUG=None
TEST=CQ

Change-Id: I9eb72d911b244eb0b176db4c86c829f337a16107
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/5650005
Reviewed-by: Samuel Jacob <samjaco@google.com>
Auto-Submit: Paz Zcharya <pazz@google.com>
Commit-Queue: Paz Zcharya <pazz@google.com>
Reviewed-by: YH Lin <yueherngl@chromium.org>
Tested-by: Paz Zcharya <pazz@google.com>

baseboard-rex/scripts/build_kernel_image.sh
@@ -12,12 +12,9 @@
 # See crrev.com/i/216896 as an example.
 
 modify_kernel_command_line() {
-  # Keep TDP MMU disabled on Rex so that ARCVM still runs normally.
-  # See b/348649388
-  echo "kvm.tdp_mmu=0" >> "$1"
-
   # Avoid a cosmetic TPM error (Work around for b/113527055)
   sed -i -e '/tpm_tis.force/d' "$1"
+
   {
     echo "tpm_tis.force=0"
 
@@ -39,6 +36,10 @@ modify_kernel_command_line() {
 
     # DPT has been implicated in stability issues on resume. The feature is of
     # limited value to ChromeOS, so disable it. See b:327056620
-    echo "i915.enable_dpt=0 " >> "$1"
+    echo "i915.enable_dpt=0"
+
+    # Keep TDP MMU disabled on Rex so that ARCVM still runs normally.
+    # See b/348649388
+    echo "kvm.tdp_mmu=0"
   } >> "$1"
 }