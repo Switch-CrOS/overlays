2ec8cfe0c0eed8359b6f02f5a15b70d326f37937
Drew Davenport <ddavenport@chromium.org>
2024-07-11 13:48:56 +0000

    Revert "rex: Disable DPT as a workaround for panic on wkae"
    This reverts commit 8065c5bc2e265431490acedbe84f6215181bdc02.
    
    Reason for revert: Upstream fix has landed, so re-enabling DPT
    
    Original change's description:
    > rex: Disable DPT as a workaround for panic on wkae
    >
    > Disabling DPT should avoid the panic on wake while Intel continues to
    > debug and address the root cause.
    >
    > BUG=b:327056620
    > TEST=suspend_stress_test with low memory
    > TEST=verify kernel command line in image built from dry run
    >
    > Cq-Depend: chrome-internal:7271614
    > Disallow-Recycled-Builds: rex-cq
    > Change-Id: I4161f9222cf4a9d990aad1fbef5a273e1453711b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/5528732
    > Commit-Queue: Drew Davenport <ddavenport@chromium.org>
    > Auto-Submit: Drew Davenport <ddavenport@chromium.org>
    > Tested-by: Drew Davenport <ddavenport@chromium.org>
    > Reviewed-by: Samuel Jacob <samjaco@google.com>
    
    BUG=b:327056620
    
    Change-Id: Ieca22469262c9b4a4f6cde19dbc6d4d5a5b05880
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/5693033
    Commit-Queue: Drew Davenport <ddavenport@chromium.org>
    Tested-by: Drew Davenport <ddavenport@chromium.org>
    Reviewed-by: Samuel Jacob <samjaco@google.com>

--- baseboard-rex/scripts/build_kernel_image.sh
+++ baseboard-rex/scripts/build_kernel_image.sh
@@ -34,10 +34,6 @@ modify_kernel_command_line() {
     # So increase the swiotlb slots from default 32768 (64MB) to 65536 (128MB)
     echo "swiotlb=65536"
 
-    # DPT has been implicated in stability issues on resume. The feature is of
-    # limited value to ChromeOS, so disable it. See b:327056620
-    echo "i915.enable_dpt=0"
-
     # Keep TDP MMU disabled on Rex so that ARCVM still runs normally.
     # See b/348649388
     echo "kvm.tdp_mmu=0"
