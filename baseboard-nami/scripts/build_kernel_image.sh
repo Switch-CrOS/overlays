44e259571783a4653a16306c5bd5cb196ef4198b
Naoya Tezuka <naoyatezuka@google.com>
2025-06-30 00:25:58 -0700

    nami: set `ecc_size` to 16 bytes for ECC for ramoops
    The default value of `ecc_size`, the size in bytes of parity date in ECC
    for ramoops, is 4 bytes. This change sets `ecc_size` to 16 bytes for
    nami, as nami devices experience more severe corruption in ramoops.
    
    In cases where `chromeos_pstore.ecc_size` already exists in the command
    line parameter, it is overwritten instead of appending a new
    entry. This change ensures that the parameter is only defined once in
    the kernel's command line.
    
    BUG=b:413292019
    TEST=build image for nami and check config.txt
    Change-Id: Iabf1111ced92755e1d5eff6be842460fd46f6ed7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/6688394
    Commit-Queue: Naoya Tezuka <naoyatezuka@google.com>
    Tested-by: Naoya Tezuka <naoyatezuka@google.com>
    Reviewed-by: Ashley Shen <shenashley@google.com>
    Reviewed-by: Tzung-Bi Shih <tzungbi@chromium.org>

--- baseboard-nami/scripts/build_kernel_image.sh
+++ baseboard-nami/scripts/build_kernel_image.sh
@@ -11,6 +11,23 @@
 
 # See crrev.com/i/216896 as an example.
 
+# Removes any existing chromeos_pstore.ecc_size parameter from the given file
+# and appends a new one with the provided value.
+#   $1: Path to the config file to modify.
+#   $2: The new value for chromeos_pstore.ecc_size.
+update_pstore_ecc_size() {
+  local file="$1"
+  local new_value="$2"
+
+  # First, remove the ecc_size parameter if it exists.
+  # The `d` command in deletes the matching line.
+  # This command does nothing if the line is not found.
+  sed -i "/^chromeos_pstore.ecc_size=/d" "${file}"
+
+  # Then, append the new ecc_size parameter.
+  echo "chromeos_pstore.ecc_size=${new_value}" >> "${file}"
+}
+
 modify_kernel_command_line() {
   # Enable S0ix validation check in kernel
   echo "intel_idle.slp_s0_check=1" >> "$1"
@@ -29,4 +46,8 @@ modify_kernel_command_line() {
 
   # Disable framebuffer compression
   echo "i915.enable_fbc=0" >> "$1"
+
+  # Set ECC parity data size to 16 bytes.
+  # See b/413292019 and go/cros-ecc-rollout for more details.
+  update_pstore_ecc_size "$1" "16"
 }
