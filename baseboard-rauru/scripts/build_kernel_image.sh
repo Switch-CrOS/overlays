1d5d0e81e6683a53d64c2a79e4b44ce2431bb49b
Yidi Lin <yidilin@google.com>
2025-10-20 14:47:22 +0800

    baseboard-rauru: Disable MTE feature
    Currently, MTE is disabled from FW. As we aim to support MTE on ALSO and
    to simplify the firmware implementation, Rauru will disable MTE on
    ChromeOS by passing `arm64.mte` kernel param through the
    `modify_kernel_command_line` script.
    
    BUG=b:438666196
    TEST=CQ
    
    Cq-Depend: chrome-internal:8684798
    Change-Id: Iacada32736e1ecad84ea4c53f1732edc6e8d6f13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/7060895
    Commit-Queue: Yidi Lin <yidilin@google.com>
    Tested-by: Yidi Lin <yidilin@google.com>
    Reviewed-by: Fei Shao <fshao@chromium.org>

--- baseboard-rauru/scripts/build_kernel_image.sh
+++ baseboard-rauru/scripts/build_kernel_image.sh
@@ -15,4 +15,5 @@ modify_kernel_command_line() {
   echo "cpuidle.governor=teo" >> "$1"
   echo "irqchip.gicv3_pseudo_nmi=1" >> "$1"
   echo "log_buf_len=524288" >> "$1"
+  echo "arm64.nomte" >> "$1"
 }
